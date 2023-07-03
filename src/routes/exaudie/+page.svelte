0d4c97095193b002880e8ef4071f50fa4e99ac41
eko setiadi <exaudie@gmail.com>
2023-07-03 17:25:08 +0700

    feat(ExaudiePage): Add page exaudie

---
+++ src/routes/exaudie/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+</script>
+
+<svelte:head>
+	<title>exaudie</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="card-wrap">
+	<Card title="Button" />
+	<Card title="Input Text" />
+</div>
+
+<style>
+	.card-wrap {
+		display: flex;
+	}
+</style>
