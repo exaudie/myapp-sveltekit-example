6dc6924daeac45d20e463c7d48393e6a6c2eafc8
eko setiadi <exaudie@gmail.com>
2023-07-27 08:53:34 +0700

    feat(Navigation): Add navigation to back

--- src/lib/components/Clicker.svelte
+++ src/lib/components/Clicker.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 
+	export let replaceClick: boolean = true;
+
 	const dispatch = createEventDispatcher();
-	const onClick = () => {
-		dispatch('onClick');
-	};
+	const onClick = () => dispatch('onClick');
 </script>
 
 <button class="{$$restProps.class || ''} button-clicker" on:click={onClick}>
-	<slot />
+	<div class:set-relative={replaceClick}>
+		<slot />
+		<div class="blocking" class:display-none={!replaceClick} />
+	</div>
 </button>
 
 <style>
 	.button-clicker {
 		all: unset;
+		cursor: pointer;
+	}
+	.set-relative {
+		position: relative;
+	}
+	.display-none {
+		display: none;
+	}
+	.blocking {
+		top: 0;
+		position: absolute;
+		width: 100%;
+		height: 100%;
 	}
 </style>
---
+++ src/lib/components/NavToBack.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { setValue } from '$lib/helpers/DefaultValue';
+	import ArrowLeft from '$lib/images/arrow-left.svg';
+	import Clicker from './Clicker.svelte';
+
+	export let label: String = '';
+	export let linkBack = '';
+
+	const toBack = () => {
+		if (setValue(linkBack) == '') return;
+
+		goto(linkBack);
+	};
+</script>
+
+<nav>
+	<Clicker on:onClick={toBack} class="clicker">
+		<img src={ArrowLeft} alt="icon" />
+	</Clicker>
+
+	<span class="font16px-bold">{label}</span>
+</nav>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	nav {
+		display: flex;
+		gap: 16px;
+		align-items: center;
+	}
+
+	nav img {
+		display: block;
+		width: 36px;
+		height: 36px;
+	}
+
+	nav span {
+		flex-grow: 1;
+	}
+
+	nav .font16px-bold {
+		font-size: 18px;
+		font-weight: 700;
+	}
+
+	nav :global(.clicker:active) {
+		background-color: grey;
+		border-radius: 4px;
+	}
+</style>
