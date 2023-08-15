676d88a6432d4fc8d2376dfb5fde6827b83ff189
eko setiadi <exaudie@gmail.com>
2023-08-16 06:47:16 +0700

    feat(Dialog): Add dialog page

---
+++ src/routes/exaudie/(pages)/dialog/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getMenuTitleFromPage } from '$lib/constants/ExaudieMenu';
+	import { page } from '$app/stores';
+	import NavToBack from '$lib/components/NavToBack.svelte';
+	import VerticalSpace from '$lib/components/VerticalSpace.svelte';
+	import Button from '$lib/components/button/Button.svelte';
+
+	const currentPath = $page.url.pathname;
+	const pathSplit = currentPath.split('/');
+
+	let dialog: HTMLDialogElement;
+</script>
+
+<NavToBack
+	label={getMenuTitleFromPage(`/${pathSplit.at(pathSplit.length - 1) ?? ''}`)}
+	linkBack="/exaudie"
+/>
+<VerticalSpace height="8px" />
+
+<Button on:onClick={() => dialog.showModal()}>show</Button>
+
