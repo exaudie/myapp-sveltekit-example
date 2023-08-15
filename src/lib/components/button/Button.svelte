ab756db9e706e1a78cf6ef0c0fb2a62bc98a7497
eko setiadi <exaudie@gmail.com>
2023-08-15 10:33:23 +0700

    refactor(Button): Update style button

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -1,1 +1,30 @@
-<button><slot /></button>
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
+
+	let dispatch = createEventDispatcher();
+
+	const onClick = () => dispatch('onClick');
+</script>
+
+<button {type} class="text-bold" on:click={onClick}><slot /></button>
+
+<style>
+	button {
+		border: 1px solid var(--text-scondary);
+		cursor: pointer;
+		border-radius: 4px;
+		padding: 0.5em 1em;
+		font-size: 1em;
+	}
+
+	button:hover {
+		border-color: blue;
+	}
+
+	button:active {
+		color: white;
+		background-color: rgba(0, 0, 255, 0.2);
+	}
+</style>
