703a292cde39dc1f442989f55acb0c92db3995d0
eko setiadi <exaudie@gmail.com>
2023-10-28 13:57:09 +0700

    refactor(Formatter): Update number formatter customize

--- src/lib/helpers/NumberFormatter.ts
+++ src/lib/helpers/NumberFormatter.ts
@@ -1,33 +1,66 @@
 import { isEmptyTo } from './DefaultValue';
 
-export const setThousandSeparator = (
+export const setIntlThousandSeparator = (
+	value: string,
+	params?: { separator?: '.' | ','; defValue?: string; toFixed?: number }
+) => {
+	if (isEmptyTo(value) == '') return params?.defValue ?? '';
+
+	const numbValue = Number(value);
+
+	if (isNaN(numbValue)) return params?.defValue ?? '';
+
+	const locales = (params?.separator ?? '.') == '.' ? 'in' : 'en';
+	const numbFormat = Intl.NumberFormat(locales, {
+		maximumFractionDigits: params?.toFixed ?? 3
+	});
+
+	return numbFormat.format(numbValue);
+};
+
+export const setCustThousandSeparator = (
 	value: string,
 	params?: { separator?: '.' | ','; defValue?: string }
 ) => {
 	if (isEmptyTo(value) == '') return params?.defValue ?? '';
 
-	const clearValue = removeSepaarator(value, { separator: params?.separator ?? '.' });
+	const separatorTo = params?.separator ?? '.';
+	const comma = separatorTo === '.' ? ',' : '.';
 
-	const newValue = clearValue;
+	const newValue = value.toString().split(comma);
+	newValue[0] = newValue[0].replace(/\B(?=(\d{3})+(?!\d))/g, separatorTo);
 
-	return newValue;
+	return newValue.join(comma);
 };
 
-export const removeSepaarator = (value: string, params?: { separator?: '.' | ',' }) => {
+export const setCust2ThousandSeparator = (
+	value: string,
+	params?: { separator?: '.' | ','; defValue?: string }
+) => {
+	if (isEmptyTo(value) == '') return params?.defValue ?? '';
+
+	const separatorTo = params?.separator ?? '.';
+	const comma = separatorTo === '.' ? ',' : '.';
+
+	const newValue = value.toString().split(comma);
+	newValue[0] = newValue[0].replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, separatorTo);
+
+	return newValue.join(comma);
+};
+
+export const removeSeparator = (value: string, params?: { separator?: '.' | ',' }) => {
 	return value.split(params?.separator ?? '.').join('');
 };
 
-export const switchSeparator = (value: string, separator?: { from: '.' | ','; to?: '.' | ',' }) => {
+export const switchSeparator = (value: string, separator?: { to?: '.' | ',' }) => {
 	if (isEmptyTo(value) == '') return '';
-	if (separator !== undefined && separator?.from === separator?.to) return '';
 
-	const separatorFrom = separator?.from ?? ',';
-	const separatorTo = separator?.to ?? (separatorFrom == ',' ? '.' : ',');
+	const separatorTo = separator?.to ?? '.';
 	const comma = separatorTo === '.' ? ',' : '.';
 
-	const splitComma = value.split(separatorFrom);
+	const splitComma = value.split(separatorTo);
 	const switchInt = splitComma[0].replaceAll(comma, separatorTo);
-	const newValue = splitComma.length > 1 ? [switchInt, splitComma[1] ?? []].join(comma) : switchInt;
+	const newValue = splitComma.length > 1 ? [switchInt, splitComma[1]].join(comma) : switchInt;
 
 	return newValue;
 };
