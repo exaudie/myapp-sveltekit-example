668ee8fb66d683d0b41c80e4b6895c20ac87f88b
eko setiadi <exaudie@gmail.com>
2023-11-13 16:43:25 +0700

    feat(Formatter): Add parse date format

--- src/routes/exaudie/(pages)/formatter/(components)/DateFormatterItem.svelte
+++ src/routes/exaudie/(pages)/formatter/(components)/DateFormatterItem.svelte
@@ -4,6 +4,8 @@
 		dateddmmmyyyyCust,
 		dateddmmyyyyCust,
 		dateyyyymmddCust,
+		getMonthENList,
+		getMonthIDList,
 		parseToDate
 	} from '$lib/helpers/DateFormatter';
 	import LabelTopLeft from '$lib/components/LabelTop.svelte';
@@ -12,6 +14,15 @@
 	import InputDate from '$lib/components/input/InputDate.svelte';
 
 	let dateValue: string = '';
+
+	const dateToFormat = {
+		date1: '13-12-2021',
+		date2: '13/12/2022',
+		date3: '13 December 2022',
+		date4: '13 Dec 2022',
+		date5: '14 Desember 2020',
+		date6: '14 Des 2020'
+	};
 </script>
 
 <LabelTopLeft label="Date Input">
@@ -41,3 +52,32 @@
 		<span>{dateyyyymmddCust(dateValue, { defaultValue: '-' })}</span>
 	</LabelTopLeft>
 </GridLayoutThreeColumn>
+
+<VerticalSpace height="48px" />
+<LabelTopLeft label="Parse Date Formatter" labelColor="var(--text-primary)">
+	<GridLayoutThreeColumn>
+		<LabelTopLeft label={dateToFormat.date1}>
+			<span>{parseToDate(dateToFormat.date1)}</span>
+		</LabelTopLeft>
+
+		<LabelTopLeft label={dateToFormat.date2}>
+			<span>{parseToDate(dateToFormat.date2)}</span>
+		</LabelTopLeft>
+
+		<LabelTopLeft label={dateToFormat.date3}>
+			<span>{parseToDate(dateToFormat.date3)}</span>
+		</LabelTopLeft>
+
+		<LabelTopLeft label={dateToFormat.date4}>
+			<span>{parseToDate(dateToFormat.date4)}</span>
+		</LabelTopLeft>
+
+		<LabelTopLeft label={dateToFormat.date5}>
+			<span>{parseToDate(dateToFormat.date5)}</span>
+		</LabelTopLeft>
+
+		<LabelTopLeft label={dateToFormat.date6}>
+			<span>{parseToDate(dateToFormat.date6)}</span>
+		</LabelTopLeft>
+	</GridLayoutThreeColumn>
+</LabelTopLeft>
