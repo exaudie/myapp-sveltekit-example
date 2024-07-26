4fea1f17fa2df717ef2c0c7fa47e9e33ddf421da
eko setiadi <exaudie@gmail.com>
2024-07-26 08:16:39 +0700

    feat(OtpPin): Add otp pin field

---
+++ src/routes/exaudie/(pages)/otp-pin/+page.server.ts
@@ -0,0 +1,46 @@
+import { fail } from '@sveltejs/kit';
+
+export const actions = {
+	confirmOtp: async ({ request }) => {
+		const data = await request.formData();
+		const otp = data.get('otp') ?? '';
+
+		await new Promise((e) => setTimeout(e, 1000));
+
+		if (otp === '123456') return { message: 'otp valid' };
+
+		return fail(400, { message: 'otp invalid' });
+	}
+};
+
+/**
+ * ActionResult example
+
+{
+  "type": "success",
+  "status": 200,
+  "data": {
+    "message": "otp valid"
+  }
+}
+
+ * 
+ 
+{
+  "type": "failure",
+  "status": 400,
+  "data": {
+    "message": "otp invalid"
+  }
+}
+
+*
+
+{
+  "type": "error",
+  "error": {
+    "message": "Internal Error"
+  },
+  "status": 500
+}
+*/
